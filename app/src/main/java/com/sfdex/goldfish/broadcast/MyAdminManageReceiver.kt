4a3b28210ced7f65bdf7839a27c17969a513b187
holden.wang <holden.wang@ucloud.cn>
2022-08-03 18:00:54 +0800

    upgrade dependencies; dpm network log

--- app/src/main/java/com/sfdex/goldfish/broadcast/MyAdminManageReceiver.kt
+++ app/src/main/java/com/sfdex/goldfish/broadcast/MyAdminManageReceiver.kt
@@ -1,9 +1,70 @@
 package com.sfdex.goldfish.broadcast
 
-import android.app.admin.DeviceAdminReceiver
+import android.app.admin.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 //设备管理器
-class MyAdminManageReceiver : DeviceAdminReceiver()
+class MyAdminManageReceiver : DeviceAdminReceiver() {
+    override fun onNetworkLogsAvailable(
+        context: Context, intent: Intent, batchToken: Long, networkLogsCount: Int
+    ) {
+
+        val dpm = getManager(context)
+        var logs: List<NetworkEvent>? = null
+
+        // Fetch the batch of logs with the batch token from the callback's arguments.
+        try {
+            logs = dpm.retrieveNetworkLogs(getWho(context), batchToken)
+        } catch (e: SecurityException) {
+            // Perhaps an unaffiliated user - handle the exception ...
+            e.printStackTrace()
+        }
+
+        // Process any logs ...
+
+        // Here, logs might be null. We can't fix because either the token doesn't match
+        // the current batch or network logging was deactivated.
+        // Confirm with isNetworkLoggingEnabled().
+
+        logs?.forEach {
+            // For this example, report the DNS hosts and discard all other data.
+            // Because we use the event ID, this example requires API level 28.
+            if (it is DnsEvent) {
+                val dnsLog = buildString {
+                    append("id: ")
+                    append(it.id)
+                    append(", time: ")
+                    append(it.timestamp)
+                    append(", hostname: ")
+                    append(it.hostname)
+                }
+                Log.d(TAG, "dnsLog: $dnsLog")
+                //reportDnsHostToServer(it.hostname, it.getTimestamp(), it.getId())
+            }
+            if (it is ConnectEvent) {
+                val connectLog = buildString {
+                    append("id: ")
+                    append(it.id)
+                    append(", time: ")
+                    append(it.timestamp)
+                    append(", port: ")
+                    append(it.port)
+                    append(", packageName: ")
+                    append(it.packageName)
+                    append(", hostName: ")
+                    append(it.inetAddress.hostName)
+                    append(", hostAddress: ")
+                    append(it.inetAddress.hostAddress)
+                    append(", canonicalHostName: ")
+                    append(it.inetAddress.canonicalHostName)
+                }
+                Log.d(TAG, "connect: $connectLog")
+            }
+        }
+    }
+}
+
+private const val TAG = "MyAdminManageReceiver"
--- app/src/main/java/com/sfdex/goldfish/utils/Utils.kt
+++ app/src/main/java/com/sfdex/goldfish/utils/Utils.kt
@@ -2,12 +2,13 @@ package com.sfdex.goldfish.utils
 
 import android.app.Activity
 import android.app.admin.DevicePolicyManager
-
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import com.sfdex.goldfish.broadcast.MyAdminManageReceiver
+import android.os.UserHandle
+import android.widget.Toast
 import com.sfdex.goldfish.R
+import com.sfdex.goldfish.broadcast.MyAdminManageReceiver
 
 
 object Utils {
@@ -18,18 +19,60 @@ object Utils {
         val mDPM = gContext.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
         //检查设备管理器权限
         if (!mDPM.isAdminActive(adminName)) {
-            setAdmin(context,adminName)
+            setAdmin(context, adminName)
         } else {
+//            mDPM.setNetworkLoggingEnabled(adminName, true)
+//            setProfileOwner(context, mDPM)
+
             mDPM.lockNow()
         }
     }
 
     //设置设备管理器
-    private fun setAdmin(context: Context,adminName: ComponentName) {
+    private fun setAdmin(context: Context, adminName: ComponentName) {
         val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
         intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, adminName);
         intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, R.string.app_name);
-        (context as Activity).startActivityForResult(intent,889);
+        (context as Activity).startActivityForResult(intent, 889);
     }
+
+    private fun setProfileOwner(context: Context, mDPM: DevicePolicyManager?) {
+        val mAdminCN = ComponentName(context, MyAdminManageReceiver::class.java)
+        if (mDPM != null) {
+            try {
+                if (mDPM.isAdminActive(mAdminCN)) {
+                    if (mDPM.isProfileOwnerApp(context.packageName)) {
+                        Toast.makeText(context, "配置管理已经激活", Toast.LENGTH_SHORT).show()
+                    } else {
+                        mDPM.setProfileEnabled(mAdminCN)
+                        //mDPM.setProfileOwner(mAdminCN, DEVICE_POLICY_TEST, UserHandle.myUserId())
+                    }
+                } else {
+                    Toast.makeText(context, "请先激活设备管理器", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: SecurityException) {
+                e.printStackTrace()
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+//    fun clearProfileOwner() {
+//        if (mDPM != null) {
+//            try {
+//                if (mDPM.isProfileOwnerApp(getPackageName())) {
+//                    mDPM.clearProfileOwner(mAdminCN)
+//                } else {
+//                    Toast.makeText(this, "配置管理已经清除", Toast.LENGTH_SHORT).show()
+//                }
+//            } catch (e: SecurityException) {
+//                e.printStackTrace()
+//            } catch (e: IllegalArgumentException) {
+//                e.printStackTrace()
+//            }
+//        }
+//    }
+
 
 }
